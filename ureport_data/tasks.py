395d9df184dcf57087699d2d3a90c2912b1cd04f
Kenneth Matovu <kbonky@gmail.com>
2015-08-13 16:50:53 +0300

    af

--- ureport_data/tasks.py
+++ ureport_data/tasks.py
@@ -33,14 +33,14 @@ def retry_if_temba_api_or_connection_error(exception):
 
 @retry(retry_on_exception=retry_if_temba_api_or_connection_error, stop_max_attempt_number=settings.RETRY_MAX_ATTEMPTS,
        wait_fixed=settings.RETRY_WAIT_FIXED)
-def fetch_entity(entity, org, n):
+def fetch_entity(entity, org, n, af=None):
     entity = entity.get('name')
     logger.info("Fetching Object of type: %s for Org: %s on Page %s", str(entity), org.name, str(n))
-    entity.fetch_objects(org, pager=TembaPager(n))
+    entity.fetch_objects(org, pager=TembaPager(n), af=af)
 
 
 @app.task
-def fetch_all(entities=None, orgs=None):
+def fetch_all(entities=None, orgs=None, af=None):
     print "Started Here"
     if not entities:
         entities = [dict(name=cls) for cls in BaseDocument.__subclasses__()]
@@ -54,7 +54,7 @@ def fetch_all(entities=None, orgs=None):
             try:
                 n = entity.get('start_page', 1)
                 while True:
-                    fetch_entity(entity, org, n)
+                    fetch_entity(entity, org, n, af=af)
                     n += 1
             except TembaException as e:
                 logger.error("Temba is misbehaving: %s - No retry", str(e))
